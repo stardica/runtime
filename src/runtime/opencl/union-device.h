53dfd9ff31657b4adf0ea6510c5c6e1a22ba7062
stardica <stardica@gmail.com>
2015-01-12 11:06:54 -0500

    initial upload

---
+++ src/runtime/opencl/union-device.h
@@ -0,0 +1,19 @@
+#ifndef __UNION_DEVICE_H__
+#define __UNION_DEVICE_H__
+
+#include <lib/util/list.h>
+
+#include <runtime/opencl/device.h>
+
+struct opencl_union_device_t
+{
+	enum opencl_runtime_type_t type;  /* First field */
+
+	struct opencl_device_t *parent;
+	struct list_t *devices;
+};
+
+struct opencl_union_device_t *opencl_union_device_create(struct opencl_device_t *parent, struct list_t *devices);
+void opencl_union_device_free(struct opencl_union_device_t *device);
+
+#endif
